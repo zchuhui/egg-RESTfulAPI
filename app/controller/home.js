5c41dec9b30c86a198f65cd4f8919308cd2dcb4e
zhuangchuhui <zhuangchuhui@iunispace.com>
2020-12-02 05:06:33 +0800

    修改欢迎

--- app/controller/home.js
+++ app/controller/home.js
@@ -2,9 +2,7 @@ const Controller = require('egg').Controller
 
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = `hi, egg-RESTfulAPI!
-    A optimized Node.js RESTful API Server Template based on egg.js.
-    https://github.com/icxcat/egg-RESTfulAPI.git`
+    this.ctx.body = '欢迎~'
   }
 }
 
